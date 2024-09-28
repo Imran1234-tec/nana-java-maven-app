8fbd43f57d133c01dfb3e6a1185da30b98eeab39
Imran1234-tec <80089584+Imran1234-tec@users.noreply.github.com>
2024-09-28 23:22:07 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,59 +1,19 @@
-// node {
-//     buildJar()
-//     buildImage()
-//     deployApp()
-// }
-
-// def buildJar() {
-//     echo "Building the application..."
-//     sh 'mvn package'
-// }
-
-// def buildImage() {
-//     echo "Building the Docker image..."
-//     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-//         sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-//         sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
-//         sh 'docker push nanajanashia/demo-app:jma-2.0'
-//     }
-// }
-
-// def deployApp() {
-//     echo 'Deploying the application...'
-// }
-
-// return this
-
-
-
-
-node {
-    buildJar()
-    buildImage()
-    deployApp()
-}
-
 def buildJar() {
-    echo "Building the application..."
+    echo "building the application..."
     sh 'mvn package'
-}
+} 
 
 def buildImage() {
-    echo "Building the Docker image..."
+    echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
+        sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push nanajanashia/demo-app:jma-2.0'
     }
-}
+} 
 
 def deployApp() {
-    echo 'Deploying the application...'
-}
+    echo 'deploying the application...'
+} 
 
-
-
-
-
-
-
+return this
