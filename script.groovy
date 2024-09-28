505289659a65071b348c87aa7406431652e08d38
Imran1234-tec <80089584+Imran1234-tec@users.noreply.github.com>
2024-09-28 23:16:25 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,3 +1,32 @@
+// node {
+//     buildJar()
+//     buildImage()
+//     deployApp()
+// }
+
+// def buildJar() {
+//     echo "Building the application..."
+//     sh 'mvn package'
+// }
+
+// def buildImage() {
+//     echo "Building the Docker image..."
+//     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//         sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+//         sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
+//         sh 'docker push nanajanashia/demo-app:jma-2.0'
+//     }
+// }
+
+// def deployApp() {
+//     echo 'Deploying the application...'
+// }
+
+// return this
+
+
+
+
 node {
     buildJar()
     buildImage()
@@ -22,4 +51,9 @@ def deployApp() {
     echo 'Deploying the application...'
 }
 
-return this
+
+
+
+
+
+
