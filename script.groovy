0a90a52419d347e2e053dd79ba8b40acb42aa858
Imran1234-tec <80089584+Imran1234-tec@users.noreply.github.com>
2024-09-28 22:21:35 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,34 +1,16 @@
 def buildJar() {
-    echo "Building the application..."
-    try {
-        def output = sh(script: 'mvn package', returnStdout: true).trim()
-        echo output
-        echo "Build successful!"
-    } catch (Exception e) {
-        echo "Build failed: ${e.getMessage()}"
-        currentBuild.result = 'FAILURE'
-        error("Stopping the build due to the failure.")
-    }
-}
-
-
+    echo "building the application..."
+    sh 'mvn package'
+} 
 
 def buildImage() {
-    echo "Building the Docker image..."
+    echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        // Verify that the Dockerfile is present
-        sh 'ls -al'
-
-        // Build the Docker image
         sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-
-        // Log in to Docker Hub
-        sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
-
-        // Push the Docker image
-        sh 'docker push imrannanajanashia/demo-app:jma-2.0'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push nanajanashia/demo-app:jma-2.0'
     }
-}
+} 
 
 def deployApp() {
     echo 'deploying the application...'
